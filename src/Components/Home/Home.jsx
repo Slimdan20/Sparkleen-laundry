7895d389946167eaed87c83fe43a5cf67533a976
Daniel Efe <daneytechwriter@gmail.com>
2025-06-30 06:08:20 -0700

    new

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,7 +4,7 @@ import Image from '../../assets/image.jpg'
 
 const Home = () => {
   return (
-    <div id='home' className='scroll-mt-20 relative h-96 bg-contain sm:h-screen sm:bg-cover' style={{ backgroundImage: `url(${Image})` }}>
+    <div id='home' className='scroll-mt-20 relative :h-screen bg-cover' style={{ backgroundImage: `url(${Image})` }}>
       <div className='absolute inset-0 bg-gradient-to-b from-deepPurple/80 to-transparent'>
       <div className='mx-5 my-14 sm:mx-24 sm:my-40 text-white leading-8'>
         <h1 className='text-3xl sm:text-6xl font-bold'>Sparkleen Laundry</h1>
